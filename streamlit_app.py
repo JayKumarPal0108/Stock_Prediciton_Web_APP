77b4f6c06d4a41fad94faf000ba6c03b937e5f2c
Jay Kumar Pal <127579215+JayKumarPal0108@users.noreply.github.com>
2024-05-28 19:08:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -265,17 +265,19 @@ with Model_Prediction:
 
         x_test = x_data[splitting_len:]
         y_test = y_data[splitting_len:]
-
         from keras.models import Sequential
-        from keras.layers import Dense, LSTM
-
+        from keras.layers import Dense, LSTM, Input
+        
         model = Sequential()
-        model.add(LSTM(128, return_sequences=True, input_shape=(x_train.shape[1],1)))
+        model.add(Input(shape=(x_train.shape[1], 1)))  # Input layer with shape
+        model.add(LSTM(128, return_sequences=True))
         model.add(LSTM(64, return_sequences=False))
         model.add(Dense(25))
         model.add(Dense(1))
+        
         model.compile(optimizer='adam', loss='mean_squared_error')
-        model.fit(x_train, y_train, batch_size=1, epochs =2)
+        model.fit(x_train, y_train, batch_size=1, epochs=2)
+        
         predictions = model.predict(x_test)
         inv_predictions = scaler.inverse_transform(predictions)
         inv_y_test = scaler.inverse_transform(y_test)
